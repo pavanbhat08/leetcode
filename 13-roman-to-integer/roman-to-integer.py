d4250b744cce43d48ddc00f66c09d26bb4bd3bfb
PAVAN N BHAT <31336294+pavanbhat08@users.noreply.github.com>
2024-06-02 16:36:16 +0530

    Time: 44 ms (70.05%) | Memory: 16.5 MB (63.94%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,42 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        num=0
-        i=0
-        while(i<len(s)):
-            if(i==len(s)-1):
-                num+=dic[s[i]]
-            elif(s[i]=="I"):
-                if(s[i+1]=="V"):
-                    num+=4
-                    i+=1
-                elif(s[i+1]=="X"):
-                    num+=9
-                    i+=1
-                else:
-                    num+=1
-            elif(s[i]=="X"):
-                if(s[i+1]=="L"):
-                    num+=40
-                    i+=1
-                elif(s[i+1]=="C"):
-                    num+=90
-                    i+=1
-                else:
-                    num+=10
-            elif(s[i]=="C"):
-                if(s[i+1]=="D"):
-                    num+=400
-                    i+=1
-                elif(s[i+1]=="M"):
-                    num+=900
-                    i+=1
-                else:
-                    num+=100
+        
+        result_number = 0 
+        prevous_number = 0
+        mapping = {'I':1, 
+            'V':5, 
+            'X':10, 
+            'L':50, 
+            'C':100, 
+            'D':500, 
+            'M':1000}
+        for symbol in s[::-1]:
+            if mapping[symbol] >= prevous_number:
+                result_number += mapping[symbol]
             else:
-                num+=dic[s[i]]
-            i+=1
-        return num
+                result_number -= mapping[symbol]
+            prevous_number = mapping[symbol]
+        return result_number
+        
 
             
             
